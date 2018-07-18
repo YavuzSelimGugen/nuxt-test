1fc6b5b70b649c14bc8b7dc94afc2647823fe800
DESKTOP-URGU1V8\Yavuz <ygugen@windowslive.com>
2018-07-18 11:31:10 +0300

    Project Started (First commit)
    Nuxt installed. Routing test are done. I sent parameter to another page.

---
+++ pages/about.vue
@@ -0,0 +1,19 @@
+<template lang="html">
+  <div class="">
+    <p @click='$router.go(-1)'>{{title}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'about',
+  data () {
+    return {
+      title: 'About Page'
+    }
+  }
+}
+</script>
+
+<style lang="css">
+</style>
---
+++ pages/user/_id.vue
@@ -0,0 +1,28 @@
+<template lang="html">
+  <div class="">
+    <p>ID: {{id}}</p>
+    <button @click='$router.go(-1)' type="button">Go Back!</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'user-id',
+  data: () => ({
+    id: 0
+  }),
+  beforeMount () {
+    let vm = this
+    vm.id = vm.$route.params.id
+    vm.loadData()
+  },
+  methods: {
+    loadData () {
+      // console.log('sss')
+    }
+  }
+}
+</script>
+
+<style lang="css">
+</style>
